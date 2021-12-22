fea98a1346c278615349a9d7067fa7777e4c1947
Hayoun <hayoun.chun@outlook.com>
2021-12-22 23:54:38 +0900

    Update Problem_Set_#1.py

--- 6.00sc/Problem_Set_#1.py
+++ 6.00sc/Problem_Set_#1.py
@@ -10,3 +10,16 @@ Principal paid = Minimum monthly payment – Interest paid
 Remaining balance = Balance – Principal paid
 '''
 
+the_outstanding_balance_on_the_credit_card = int(raw_input('Enter the outstanding balance on your credit card:'))
+annual_interest_rate = float(raw_input('Enter the annual credit card interest rate as a decimal:'))
+minimum_monthly_payment_rate = float(raw_input('Enter the minimum monthly payment rate as a decimal'))
+print int(the_outstanding_balance_on_the_credit_card)
+print float(annual_interest_rate)
+print float(minimum_monthly_payment_rate)
+minimum_monthly_payment = minimum_monthly_payment_rate * the_outstanding_balance_on_the_credit_card
+print float(minimum_monthly_payment)
+interest_Paid = annual_interest_rate / 12 * the_outstanding_balance_on_the_credit_card
+principal_paid = minimum_monthly_payment - interest_Paid
+remaining_balance = the_outstanding_balance_on_the_credit_card - principal_paid
+print float(principal_paid)
+print float(remaining_balance)
