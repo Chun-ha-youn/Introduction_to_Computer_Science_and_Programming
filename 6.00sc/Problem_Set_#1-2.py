9b2215c49eaaff694648bd17ae240d45226f67bf
Hayoun <hayoun.chun@outlook.com>
2021-12-25 19:13:17 +0900

    Update Problem_Set_#1-2.py
    Add

--- 6.00sc/Problem_Set_#1-2.py
+++ 6.00sc/Problem_Set_#1-2.py
@@ -12,18 +12,28 @@ the_outstanding_balance_on_the_credit_card = int(raw_input('Enter the outstandin
 annual_interest_rate = float(raw_input('Enter the annual credit card interest rate as a decimal:'))
 monthly_interest_rate = annual_interest_rate / 12.0
 
-monthly_payment = 10
 month = 0
+updated_balance_each_month = the_outstanding_balance_on_the_credit_card
 
-while the_outstanding_balance_on_the_credit_card >0:
-    if month < 12:
-        the_outstanding_balance_on_the_credit_card = the_outstanding_balance_on_the_credit_card * (1 + monthly_interest_rate) - monthly_payment
-        print float(the_outstanding_balance_on_the_credit_card)
-        print int(monthly_payment)
-        month += 1
-        print int(month)
-    if month == 12:
-        print 'finish'
-        month += 1
-        monthly_payment += 10
-        print int(monthly_payment)
+for monthly_payment in range(10, the_outstanding_balance_on_the_credit_card+10):
+    while the_outstanding_balance_on_the_credit_card > 0:
+        if month < 12:
+            updated_balance_each_month = updated_balance_each_month * (1 + monthly_interest_rate) - monthly_payment
+            month += 1
+            if updated_balance_each_month < 0:
+                print float(updated_balance_each_month)
+                print float(monthly_payment)
+                print int(month)
+                break
+        elif month == 12:
+            month += 1
+            if updated_balance_each_month > 0:
+                updated_balance_each_month = the_outstanding_balance_on_the_credit_card
+                month = 0
+                monthly_payment += 10
+                if updated_balance_each_month < 0:
+                    print float(updated_balance_each_month)
+                    print float(monthly_payment)
+                    print int(month)
+                    break
+    break
